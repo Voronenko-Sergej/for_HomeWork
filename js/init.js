e9125612571913cd6430bc61bb716abaa8215fae
Voronenko-Sergej <serghiy2d@gmail.com>
2017-01-22 00:10:59 +0300

    Домашнее задание

---
+++ js/init.js
@@ -0,0 +1,19 @@
+$('.open-order-modal').on('click', function () {
+    $('.modal-order').bPopup({
+        closeClass: 'close',
+    });
+});
+
+
+ $('select').styler();
+
+$('.carusel-ubautUs').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+});
+		
+				
